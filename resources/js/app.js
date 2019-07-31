ce912b1f9ab40e9fb472cc38dbb79bbef73725ad
paponkoushik <paponkoushik@gmail.com>
2019-07-31 19:13:10 +0600

    vue router has been used. using const router

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,18 +11,9 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -32,4 +23,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
