435be0a306429eeecbc00a99289bae9d39999082
paponkoushik <paponkoushik@gmail.com>
2019-08-02 02:39:05 +0600

    router-link for vue js using here for locate which route should it go

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
                     <a class="dropdown-item" href="#">Settings</a>
                     <a class="dropdown-item" href="#">Activity Log</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                    <router-link to="/logout" class="dropdown-item">Logout</router-link>
                 </div>
             </li>
         </ul>
