d3be48ed23e0353ec3ce69750e0d5ddc3f4ed259
paponkoushik <paponkoushik@gmail.com>
2019-08-01 19:04:53 +0600

    User class imported

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
 require('./bootstrap');
 
@@ -13,16 +8,15 @@ Vue.use(VueRouter)
 //import routes
 import {routes} from './routes';
 
+//import user class
+
+import User from './helper/user';
+window.User = User;
+
 const router = new VueRouter({
     routes,
     mode :'history'
 })
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app',
