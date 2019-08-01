678daa5ebdccd00ae021df69aefd11bcdeec4507
paponkoushik <paponkoushik@gmail.com>
2019-08-02 02:39:34 +0600

    A new route has been created for logout

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,6 +2,7 @@
 let login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 let forget = require('./components/auth/forget.vue').default;
+let logout = require('./components/auth/logout.vue').default;
 
 let home = require('./components/home.vue').default;
 
@@ -9,5 +10,6 @@ export const routes = [
     { path: '/', component: login, name:'login' },
     { path: '/register', component: register, name:'register' },
     { path: '/forget', component: forget, name:'forget' },
+    { path: '/logout', component: logout , name:'logout' },
     { path: '/home', component: home , name:'home' },
 ];
