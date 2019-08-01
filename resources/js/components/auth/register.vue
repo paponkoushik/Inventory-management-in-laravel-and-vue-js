ecde82e05e539e01f40b284d6c7f2e8adc6d82e6
paponkoushik <paponkoushik@gmail.com>
2019-08-02 02:12:53 +0600

    Signup form working all the vue code included.

--- resources/js/components/auth/register.vue
+++ resources/js/components/auth/register.vue
@@ -2,16 +2,18 @@
     <div class="card card-register mx-auto mt-5">
         <div class="card-header">Register an Account</div>
         <div class="card-body">
-            <form>
+            <form @submit.prevent = "signup">
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="email" name="name" id="inputFullName" class="form-control" placeholder="Full Name" required="required">
+                        <input type="text" v-model="form.name" id="inputFullName" class="form-control" placeholder="Full Name">
+                        <small class="text-danger" v-if="errors.name" style="color: red;"> {{ errors.name[0] }} </small>
                         <label for="inputFullName">Full Name</label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required="required">
+                        <input type="email" v-model="form.email" id="inputEmail" class="form-control" placeholder="Email address">
+                        <small class="text-danger" v-if="errors.email" style="color: red;"> {{ errors.email[0] }} </small>
                         <label for="inputEmail">Email address</label>
                     </div>
                 </div>
@@ -19,13 +21,14 @@
                     <div class="form-row">
                         <div class="col-md-6">
                             <div class="form-label-group">
-                                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
+                                <input type="password" v-model="form.password" id="inputPassword" class="form-control" placeholder="Password">
+                                <small class="text-danger" v-if="errors.password" style="color: red;"> {{ errors.password[0] }} </small>
                                 <label for="inputPassword">Password</label>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-label-group">
-                                <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
+                                <input type="password" v-model="form.password_confirmation" id="confirmPassword" class="form-control" placeholder="Confirm password">
                                 <label for="confirmPassword">Confirm password</label>
                             </div>
                         </div>
@@ -41,7 +44,39 @@
 </template>
 
 <script type="text/javascript">
+    export default {
+        created(){
+            if (User.loggedIn()) {
+                this.$router.push({name : 'home'})
+            }
+        },
+        data(){
+            return {
+                form:{
+                    name: null,
+                    email: null,
+                    password: null,
+                    confirm_password: null
+                },
+                errors:{},
+            }
+        },
+        methods:{
+            signup(){
+                axios.post('/api/auth/signup',this.form)
+                    .then(res => {
+                        User.responseAfterLogin(res)
+                        Toast.fire({
+                            type: 'success',
+                            title: 'Signed in successfully'
+                        })
+                        this.$router.push({ name : 'home'})
+                    })
+                    .catch(error => this.errors = error.response.data.errors)
 
+            }
+        }
+    }
 </script>
 
 <style scoped>
