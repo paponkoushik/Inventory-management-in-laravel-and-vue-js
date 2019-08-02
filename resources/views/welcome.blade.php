29e16980b739969f5c5f38b235d1ef7dddb7f62c
paponkoushik <paponkoushik@gmail.com>
2019-08-03 01:43:57 +0600

    dashboard redirect to /home and  add employee redirect to employee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,31 +90,32 @@
     <div id="wrapper">
         <ul class="sidebar navbar-nav" id="leftbar" v-show="$route.path === '/' || $route.path === '/register' || $route.path ==='/forget' ? false : true "  style="display: none;">
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <router-link class="nav-link" to="/home">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span>
-                </a>
+                </router-link>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+                    <span>Employee</span>
                 </a>
                 <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                    <h6 class="dropdown-header">Login Screens:</h6>
-                    <a class="dropdown-item" href="login.html">Login</a>
-                    <a class="dropdown-item" href="register.html">Register</a>
-                    <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
-                    <div class="dropdown-divider"></div>
-                    <h6 class="dropdown-header">Other Pages:</h6>
-                    <a class="dropdown-item" href="404.html">404 Page</a>
-                    <a class="dropdown-item" href="blank.html">Blank Page</a>
+                    <router-link class="dropdown-item" to="employee">Add New</router-link>
+                    <router-link class="dropdown-item" to="#">All Employee</router-link>
+
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Supplier</span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <router-link class="dropdown-item" to="#">All Supplier</router-link>
+                    <router-link class="dropdown-item" to="#">Add Supplier</router-link>
+
+                </div>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="tables.html">
