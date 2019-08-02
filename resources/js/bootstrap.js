de3a4e0507efb855560356d578897f99835a33cd
paponkoushik <paponkoushik@gmail.com>
2019-08-02 20:08:58 +0600

    Noty loaded in boostrap.js file from the noty installation documentation.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,8 @@
 window._ = require('lodash');
 
+/*noty loaded here*/
+window.Noty = require('noty');
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
