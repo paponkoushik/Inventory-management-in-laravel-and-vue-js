5aaa251d4c5cbf32b7506806f4004d9a70b9d897
paponkoushik <paponkoushik@gmail.com>
2019-08-03 01:19:28 +0600

    For token meta tag should be needed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <head>
 
     <meta charset="utf-8">
+    {{--must be include for token--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{--End--}}
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
