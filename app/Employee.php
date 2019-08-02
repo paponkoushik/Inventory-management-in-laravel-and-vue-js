940a626e1ed7f1eca32b3f03a92e97a81a6c43d8
paponkoushik <paponkoushik@gmail.com>
2019-08-03 03:19:09 +0600

    Fillable fields are selected.

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'email', 'phone', 'address', 'nid', 'photo', 'joining_date'
+    ];
 }
