49855721cb3cb6221fd7c81bb60a79b238b7fd8c
paponkoushik <paponkoushik@gmail.com>
2019-08-03 02:40:03 +0600

    employees table created

--- database/migrations/2019_08_02_203510_create_employees_table.php
+++ database/migrations/2019_08_02_203510_create_employees_table.php
@@ -15,6 +15,13 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('photo')->nullable();
+            $table->string('nid')->nullable();
+            $table->string('joining_date');
             $table->timestamps();
         });
     }
