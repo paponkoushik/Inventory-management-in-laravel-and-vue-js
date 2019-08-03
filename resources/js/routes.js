59927a16eed520db6a944dc147accd323103c5c9
paponkoushik <paponkoushik@gmail.com>
2019-08-04 04:59:53 +0600

    Route name changed

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -8,15 +8,21 @@ let logout = require('./components/auth/logout.vue').default;
 /*Employee route*/
 let storeemployee = require('./components/employee/CreateEmployee.vue').default;
 let employee = require('./components/employee/EmployeeList.vue').default;
+let editemployee = require('./components/employee/EditEmployee.vue').default;
+/*End employee route*/
 
 let home = require('./components/home.vue').default;
 
 export const routes = [
-    { path: '/', component: login, name:'login' },
+    { path: '/', component: login, name:'/' },
     { path: '/register', component: register, name:'register' },
     { path: '/forget', component: forget, name:'forget' },
-    { path: '/logout', component: logout , name:'logout' },
-    { path: '/store-employee', component: storeemployee , name:'store-employee' },
-    { path: '/employee', component: employee , name:'employee' },
+    { path: '/logout', component: logout, name:'logout' },
+
+    //employee routes
+    { path: '/store-employee', component: storeemployee, name:'store-employee' },
+    { path: '/employee', component: employee, name:'employee' },
+    { path: '/edit-employee/:id', component: editemployee , name:'edit-employee' },
+    //end employee routes
     { path: '/home', component: home , name:'home' },
 ];
